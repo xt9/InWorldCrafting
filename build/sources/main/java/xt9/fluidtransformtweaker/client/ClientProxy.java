0cb01fc1c6aacb646de4a56cbd9086934ac09b3e
Niklas Andersson <niklas_andersson90@hotmail.com>
2019-01-19 12:49:44 +0100

    Forgot to add gitignore

--- build/sources/main/java/xt9/fluidtransformtweaker/client/ClientProxy.java
+++
@@ -1,9 +0,0 @@
-package xt9.fluidtransformtweaker.client;
-
-import xt9.fluidtransformtweaker.common.CommonProxy;
-
-/**
- * Created by xt9 on 2019-01-12.
- */
-public class ClientProxy extends CommonProxy {
-}
